2a661699bc6d4ef8ec1cd46dbb5f420190e5b82c
sabesansathananthan <sabesan.16@cse.mrt.ac.lk>
2020-10-24 23:37:13 +0530

    update: vesion

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { LandingPage, Resume } from "./pages";
 import { NotFoundPage } from "./components";
+import { Grid } from "@material-ui/core";
 
 const App = () => (
   <Router basename={process.env.PUBLIC_URL}>
-    <div id="app">
+    <Grid id="app">
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/user/:username/resume" component={Resume} />
         <Route path="/404-not-found" component={NotFoundPage} />
       </Switch>
-    </div>
+    </Grid>
   </Router>
 );
 
